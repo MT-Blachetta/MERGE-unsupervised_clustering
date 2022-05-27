dbb7810dba1b35b03480d62c20f940fa48588959
MT-Blachetta <michaelblachetta@gmail.com>
2022-05-27 17:31:35 +0200

    Update scan_dataset.py

--- dataset/scan_dataset.py
+++ dataset/scan_dataset.py
@@ -67,7 +67,7 @@ class NeighborsDataset(Dataset):
         self.dataset = dataset
         self.indices = indices # Nearest neighbor indices (np.array  [len(dataset) x k])
         print('len [dataset]: ',len(self.dataset))
-        print('indices.shape[0]: ',len(self.indices.shape[0]))
+        print('indices.shape[0]: ',self.indices.shape[0])
         if num_neighbors is not None:
             self.indices = self.indices[:, :num_neighbors+1]
         assert(self.indices.shape[0] == len(self.dataset))
