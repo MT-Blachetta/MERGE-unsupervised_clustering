9514c160e7a2caeeab425441888ecb1ffe9d3c78
MT-Blachetta <michaelblachetta@gmail.com>
2022-07-30 02:02:24 +0200

    Update training.py

--- training.py
+++ training.py
@@ -535,7 +535,7 @@ def fixmatch_train(device,model,labeled_loader,unlabeled_loader,consistency_tens
         u_batch = unlabeled_iter.next()
         indices = u_batch['index']
         indices.to(device)
-        consistencies.to(device)
+        consistency_tensor.to(device)
         weak_images = u_batch['weak_image']
         strong_images = u_batch['strong_image']
 
